3ea7b00d168818663bd44c5a781df94b38a22f1f
ajai1 <emailtoajai@gmail.com>
2018-11-10 20:31:35 +0530

    added another anime

--- main.js
+++ main.js
@@ -15,5 +15,32 @@ var playPause = anime({
     loop:true
   });
 
+  var enlargeThisBox = anime ({
+    targets: '#enlargebox',
+    translateX: {
+      value: 700,
+      duration: 800
+    },
+    rotate: {
+        value: 360,
+        duration: 1800,
+        easing: 'easeInOutSine'
+      },
+      scale: {
+        value: 2,
+        duration: 1600,
+        delay: 800,
+        easing: 'easeInOutQuart'
+      },
+      delay: 250,
+      autoplay: true,
+    loop:true
+  });
+
+  function enlargeBox() {
+    enlargeThisBox.play;
+  }
+
+
   document.querySelector('#boxes .play').onclick = playPause.play;
   document.querySelector('#boxes .pause').onclick = playPause.pause;
